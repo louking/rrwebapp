9185bf9c3db03f822d126ce53aa8da7e111868d9
Lou King <lking@pobox.com>
2016-06-12 06:20:49 -0400

    refactor fabfile
    * common deploy function
    * load scripts from ../libs/*

--- fabfile.py
+++ fabfile.py
@@ -4,21 +4,23 @@ USERNAME = 'scoretility'
 APP_NAME = 'rrwebapp'
 WSGI_SCRIPT = 'rrwebapp.wsgi'
 
-def deployproduction():
+def deploy(project_dir, branchname):
+    with cd(project_dir):
+        run('git pull')
+        run('git checkout {}'.format(branchname))
+        run('cp -R ../libs/js  rrwebapp/static')
+        run('cp -R ../libs/css rrwebapp/static')
+        run('source bin/activate; pip install -r requirements.txt')
+        run('touch %s' % WSGI_SCRIPT)
+
+def deployproduction(branchname='master'):
     server = 'scoretility.com'
     project_dir = '/var/www/%s/html/%s' % (server, APP_NAME)
     env.hosts = ["%s@%s" % (USERNAME, server)]
-    with cd(project_dir):
-        run('git pull')
-        run('source bin/activate; pip install -r requirements.txt')
-        run('touch %s' % WSGI_SCRIPT)
+    deploy(project_dir, branchname)
 
 def deploybeta(branchname='master'):
     server = 'beta.scoretility.com'
     project_dir = '/var/www/%s/html/%s' % (server, APP_NAME)
     env.hosts = ["%s@%s" % (USERNAME, server)]
-    with cd(project_dir):
-        run('git pull')
-        run('git checkout {}'.format(branchname))
-        run('source bin/activate; pip install -r requirements.txt')
-        run('touch %s' % WSGI_SCRIPT)
+    deploy(project_dir, branchname)
