3e87899c93622981ef53e170f68842afc4f38d07
Lou King <lking@pobox.com>
2019-07-25 14:21:40 -0400

    upgrade to fab 2.4.0
    * fixes #365

--- fabfile.py
+++ fabfile.py
@@ -1,48 +1,65 @@
 ###########################################################################################
 # fabfile  -- deployment using Fabric
 #
-#   Copyright 2014 Lou King
+#   Copyright 2019 Lou King
 ###########################################################################################
 '''
 fabfile  -- deployment using Fabric
 =================================================================
 
+expecting fabric.json with following content
+    {
+        "connect_kwargs": {
+            "key_filename": sshkeyfilename (export OpenSSH key from puttygen)
+        },
+        "user": "{appname}mgr" (meaning account to log in for this app)
+    }
+
+execute as follows
+
+    fab -H <target-host> deploy
+
+or 
+
+    fab -H <target1>,<target2> deploy
+
+if you need to check out a particular branch
+
+    fab -H <target-host> deploy --branchname=<branch>
 '''
 
-from fabric.api import env, run, cd
+from fabric import task
+from invoke import Exit
 
-USERNAME = 'scoretility'
 APP_NAME = 'rrwebapp'
 WSGI_SCRIPT = 'rrwebapp.wsgi'
+JS_SOURCE = '/home/scoretility/devhome/js'
 
-project_dir = ''
+@task
+def deploy(c, branchname='master'):
+    print('c.user={} c.host={} branchname={}'.format(c.user, c.host, branchname))
 
-def sandbox():
-    server = 'sandbox.scoretility.com'
-    global project_dir
-    project_dir = '/var/www/{}/{}'.format(server, APP_NAME)
-    env.hosts = ["{}@{}".format(USERNAME, server)]
+    # TODO: rrwebapp should be venv, separate from application, fix for python 3
+    venv_dir = '/var/www/{server}/rrwebapp'.format(server=c.host)
+    project_dir = '/var/www/{server}/{appname}'.format(server=c.host, appname=APP_NAME)
 
-def beta():
-    server = 'beta.scoretility.com'
-    global project_dir
-    project_dir = '/var/www/{}/{}'.format(server, APP_NAME)
-    env.hosts = ["{}@{}".format(USERNAME, server)]
+    c.run('cd {} && git pull'.format(project_dir))
 
-def prod():
-    server = 'scoretility.com'
-    global project_dir
-    project_dir = '/var/www/{}/{}'.format(server, APP_NAME)
-    env.hosts = ["{}@{}".format(USERNAME, server)]
+    if not c.run('cd {} && git show-ref --verify --quiet refs/heads/{}'.format(project_dir, branchname), warn=True):
+        raise Exit('branchname {} does not exist'.format(branchname))
 
-def deploy(branchname='master'):
-    with cd(project_dir):
-        run('git pull')
-        run('git checkout {}'.format(branchname))
-        run('cp -R ../libs/js  rrwebapp/static')
-        run('cp -R ../libs/css rrwebapp/static')
-        # must source bin/activate before each command which must be done under venv
-        # because each is a separate process
-        run('source bin/activate; pip install -r requirements.txt')
-        run('source bin/activate; alembic -c rrwebapp/alembic.ini upgrade head')
-        run('touch {}'.format(WSGI_SCRIPT))
+    c.run('cd {} && git checkout {}'.format(project_dir, branchname))
+    # NOTE: this may be application specific
+    c.run('cd {project_dir} && cp -R {js_source} {appname}/static'.format(project_dir=project_dir, appname=APP_NAME, js_source=JS_SOURCE))
+    # must source bin/activate before each command which must be done under venv
+    # because each is a separate process
+    c.run('cd {} && source {}/bin/activate && pip install -r requirements.txt'.format(project_dir, venv_dir))
+
+    versions_dir = '{project_dir}/{appname}/versioning/versions'.format(project_dir=project_dir, appname=APP_NAME)
+    if not c.run('test -d {}'.format(versions_dir), warn=True):
+        c.run('mkdir -p {}'.format(versions_dir))
+
+    c.run('cd {project_dir} && source {venv_dir}/bin/activate && alembic -c {appname}/alembic.ini upgrade head'.format(project_dir=project_dir,
+                                                                                                                       appname=APP_NAME,
+                                                                                                                       venv_dir=venv_dir))
+    c.run('cd {} && touch {}'.format(project_dir, WSGI_SCRIPT))
