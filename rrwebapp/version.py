a41a07ab959c9ff1d579ad53109240fc0897f002
Lou King <lking@pobox.com>
2021-07-26 15:29:26 -0400

    docs folder removed

--- rrwebapp/version.py
+++ rrwebapp/version.py
@@ -1,5 +1,5 @@
 # this string is used for the version string in the documentation
-__version__ = '3.0.1.0'
+__version__ = '3.0.1.1.dev1'
 __docversion__ = __version__
 # uncomment for development
 # __docversion__ = 'latest'
