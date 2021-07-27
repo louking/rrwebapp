caaf8c4e4f795b4c01b5286f80b45d8a8bb3cf03
Lou King <lking@pobox.com>
2021-07-27 16:20:53 -0400

    bump version

--- rrwebapp/version.py
+++ rrwebapp/version.py
@@ -1,6 +1,6 @@
 # this string is used for the version string in the documentation
-__version__ = '3.0.1.1'
+__version__ = '3.0.1.2.dev1'
 __docversion__ = __version__
 # uncomment for development
-# __docversion__ = 'latest'
+__docversion__ = 'latest'
 
