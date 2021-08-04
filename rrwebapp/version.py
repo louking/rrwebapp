ac147c9e0c2c88d7dc6cb67f94b5114b20bb4127
Lou King <lking@pobox.com>
2021-08-04 06:57:24 -0400

    version 3.0.2.0

--- rrwebapp/version.py
+++ rrwebapp/version.py
@@ -1,6 +1,6 @@
 # this string is used for the version string in the documentation
-__version__ = '3.0.1.3.dev1'
+__version__ = '3.0.2.0'
 __docversion__ = __version__
 # uncomment for development
-__docversion__ = 'latest'
+# __docversion__ = 'latest'
 
